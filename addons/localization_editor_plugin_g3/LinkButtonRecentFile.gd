960b08780d837b6c65f71973c089776db525ede2
Ethan <e_q101@live.com>
2024-02-12 17:10:56 -0600

    Detect when recent files cannot be found

--- addons/localization_editor_plugin_g3/LinkButtonRecentFile.gd
+++ addons/localization_editor_plugin_g3/LinkButtonRecentFile.gd
@@ -7,13 +7,19 @@ signal removed(NodeName,f_path)
 var f_path:String
 
 func _ready() -> void:
+	var full_path : String = "%s/%s" % [
+		f_path.get_base_dir(),
+		f_path.get_file()
+	] 
+	if not FileAccess.file_exists(full_path):
+		$Lnk.disabled = true
 	$Lnk.text = "%s" % [
 		f_path.get_file()
 	]
 	
-	$Lnk.tooltip_text = "%s/%s" % [
-		f_path.get_base_dir(),
-		f_path.get_file()
+	$Lnk.tooltip_text = "%s%s" % [
+		"[NOT FOUND] " if $Lnk.disabled else "",
+		full_path
 	] 
 
 
