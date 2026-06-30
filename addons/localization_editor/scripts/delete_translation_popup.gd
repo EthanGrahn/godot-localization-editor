9bbe87df3c5826fd862e23a5a6ae83a516571515
Ethan Grahn <e_q101@live.com>
2026-06-30 16:14:17 -0500

    Further refine UI elements (#81)
    * Add an open file tab button
    
    * Limit to unused languages in add language popup
    
    * Add a menu option to open recent files
    
    * Improve handling of file reopening
    
    * Improve search layout

--- addons/localization_editor/scripts/delete_translation_popup.gd
+++ addons/localization_editor/scripts/delete_translation_popup.gd
@@ -3,7 +3,7 @@ extends ConfirmationDialog
 
 signal delete_confirmed(remember_choice: bool)
 
-@onready var _remember_checkbox: CheckButton = $VBoxContainer/HBoxContainer/CheckButton
+@onready var _remember_checkbox: CheckBox = $VBoxContainer/HBoxContainer/CheckBox
 
 
 func _on_confirmed() -> void:
--- addons/localization_editor/scripts/dock.gd
+++ addons/localization_editor/scripts/dock.gd
@@ -11,9 +11,6 @@ const _SETTINGS_FILE: String = "user://settings.ini"
 @export var _prefs_lang_option: OptionButton
 @export var _ref_lang_option: OptionButton
 @export var _target_lang_option: OptionButton
-@export var _cb_search_key: CheckBox
-@export var _cb_search_ref_text: CheckBox
-@export var _cb_search_target_text: CheckBox
 
 @onready var _config_manager: Node = get_tree().root.find_child("ConfigManager", true, false)
 @onready var _no_files_panel: Control = %ControlNoOpenedFiles
@@ -21,7 +18,7 @@ const _SETTINGS_FILE: String = "user://settings.ini"
 @onready var _open_file_popup: Popup = $OpenFilePopup
 @onready var _create_file_popup: Popup = $CreateFilePopup
 @onready var _credits_popup: Popup = $CreditsWindow
-@onready var _version_label: Label = $Dock/VBoxContainer/LblTextBottom
+@onready var _version_label: Label = $Dock/VBoxContainer/HBoxContainer/LblTextBottom
 @onready var _add_lang_popup: Popup = $AddLangPopup
 @onready var _remove_lang_popup: Popup = $RemoveLangPopup
 @onready var _add_translation_popup: Popup = $AddTranslationPopup
@@ -41,12 +38,17 @@ const _SETTINGS_FILE: String = "user://settings.ini"
 
 var _is_config_initialized := false
 var _save_pressed := false
+var _find_pressed := false
 var _save_warning_dialog: ConfirmationDialog
 var _autosave_timer: Timer
 var _recovery_dialog: ConfirmationDialog
 var _pending_recovery_temp: String = ""
 var _pending_open_path: String = ""
 var _pending_open_delimiter: String = ""
+var _recent_files_submenu: PopupMenu
+var _recent_files_cache: Array = []
+
+const _PLUS_TAB_META = "__plus_tab__"
 
 var _current_data: Dictionary
 var _translations: Dictionary:
@@ -78,7 +80,13 @@ var _current_path_config := ConfigFile.new()
 var _file_states: Dictionary = {}  # path -> { data, ref_lang_idx, target_lang_idx, is_dirty }
 var _switching_tabs: bool = false
 var _google_translate: Node
-var _search_filters := {"need_translation": false, "need_revision": false}
+var _search_filters := {
+	"need_translation": false,
+	"need_revision": false,
+	"search_key": true,
+	"search_ref_text": true,
+	"search_target_text": true
+}
 
 
 func _ready() -> void:
@@ -117,17 +125,36 @@ func _ready() -> void:
 	_recovery_dialog.get_ok_button().text = "Recover"
 	_recovery_dialog.get_cancel_button().text = "Discard"
 
-	_menu_file.get_popup().id_pressed.connect(_on_file_menu_id_pressed)
+	var file_popup := _menu_file.get_popup()
+	file_popup.add_item("New", 1)
+	file_popup.add_item("Open", 2)
+	_recent_files_submenu = PopupMenu.new()
+	_recent_files_submenu.name = "OpenRecentSubmenu"
+	_recent_files_submenu.index_pressed.connect(_on_recent_file_selected)
+	file_popup.add_child(_recent_files_submenu)
+	file_popup.add_submenu_node_item("Open Recent", _recent_files_submenu, 5)
+	file_popup.add_separator()
+	file_popup.add_item("Save", 3)
+	file_popup.add_item("Close All", 4)
+	file_popup.id_pressed.connect(_on_file_menu_id_pressed)
+	file_popup.about_to_popup.connect(_refresh_recent_files_submenu)
 	_menu_edit.get_popup().id_pressed.connect(_on_edit_menu_id_pressed)
 	_menu_help.get_popup().id_pressed.connect(_on_help_menu_id_pressed)
 
 	_search_filter_popup.hide_on_item_selection = false
 	_search_filter_popup.hide_on_checkable_item_selection = false
 	_search_filter_popup.allow_search = false
+	_search_filter_popup.add_separator("Display Filters")
 	_search_filter_popup.add_check_item("Needs translation", 0)
 	_search_filter_popup.add_check_item("Needs revision", 1)
+	_search_filter_popup.add_separator("Search Filters")
+	_search_filter_popup.add_check_item("Key", 2)
+	_search_filter_popup.add_check_item("Reference", 3)
+	_search_filter_popup.add_check_item("Target", 4)
+	_search_filter_popup.set_item_checked(4, true)
+	_search_filter_popup.set_item_checked(5, true)
+	_search_filter_popup.set_item_checked(6, true)
 	_search_filter_popup.index_pressed.connect(_on_filter_changed)
-	_search_filter_popup.index_pressed.connect(_search_filter_popup.toggle_item_checked)
 
 	_close_all()
 
@@ -148,11 +175,13 @@ func _ready() -> void:
 
 	_no_files_panel.initialize(_config_manager)
 
-	# reopen the last file
+	# reopen files that were open in the last session
 	if _config_manager.get_settings_value("main", "reopen_last_file", false):
-		var last_file: String = _no_files_panel.get_most_recent_file()
-		if not last_file.is_empty() and FileAccess.file_exists(last_file):
-			_open_file(last_file)
+		var open_files: Array = _config_manager.get_settings_value("main", "open_files", [])
+		for file_path: String in open_files:
+			if FileAccess.file_exists(file_path):
+				_open_file(file_path)
+				await _vbx_translations.list_ready
 
 
 func _process(_delta):
@@ -162,26 +191,31 @@ func _process(_delta):
 	elif _save_pressed and not Input.is_key_pressed(KEY_S):
 		_save_pressed = false
 
+	if not _find_pressed and Input.is_key_pressed(KEY_CTRL) and Input.is_key_pressed(KEY_F):
+		_line_edit_search_box.grab_focus()
+		_line_edit_search_box.select_all()
+		_find_pressed = true
+	elif _find_pressed and not Input.is_key_pressed(KEY_F):
+		_find_pressed = false
+
 
 func _start_search() -> void:
 	var search_text: String = _line_edit_search_box.text.strip_edges().to_lower()
-	var hide_translated: bool = _search_filters["need_translation"]
-	var hide_no_need_rev: bool = _search_filters["need_revision"]
-	_search_filters["search_key"] = _cb_search_key.button_pressed
-	_search_filters["search_ref_text"] = _cb_search_ref_text.button_pressed
-	_search_filters["search_target_text"] = _cb_search_target_text.button_pressed
 	_vbx_translations.search(search_text, _search_filters)
 
 
 func _clear_search() -> void:
 	_line_edit_search_box.text = ""
-	_cb_search_key.button_pressed = true
-	_cb_search_ref_text.button_pressed = true
-	_cb_search_target_text.button_pressed = true
 	_search_filters["need_translation"] = false
 	_search_filters["need_revision"] = false
-	for i in range(0, _search_filter_popup.item_count):
-		_search_filter_popup.set_item_checked(i, false)
+	_search_filters["search_key"] = true
+	_search_filters["search_ref_text"] = true
+	_search_filters["search_target_text"] = true
+	_search_filter_popup.set_item_checked(1, false)
+	_search_filter_popup.set_item_checked(2, false)
+	_search_filter_popup.set_item_checked(4, true)
+	_search_filter_popup.set_item_checked(5, true)
+	_search_filter_popup.set_item_checked(6, true)
 
 
 func alert(txt: String, title: String = "Alert!") -> void:
@@ -237,7 +271,7 @@ func _on_edit_menu_id_pressed(id: int) -> void:
 		1:
 			# add new language
 			if _no_files_panel.visible == false:
-				_add_lang_popup.popup_centered()
+				_add_lang_popup.request_popup(_langs)
 		2:
 			# delete language
 			if _no_files_panel.visible == false and _langs.size() > 0:
@@ -260,10 +294,38 @@ func _on_help_menu_id_pressed(id: int) -> void:
 			_credits_popup.popup_centered()
 
 
+func _refresh_recent_files_submenu() -> void:
+	_recent_files_submenu.clear()
+	if not is_instance_valid(_config_manager) or not _config_manager.is_initialized:
+		_recent_files_submenu.add_item("No recent files")
+		_recent_files_submenu.set_item_disabled(0, true)
+		return
+	var recent: PackedStringArray = _config_manager.get_settings_value("main", "recent_files", [])
+	_recent_files_cache = Array(recent.slice(0, 5))
+	if _recent_files_cache.is_empty():
+		_recent_files_submenu.add_item("No recent files")
+		_recent_files_submenu.set_item_disabled(0, true)
+		return
+	for path: String in _recent_files_cache:
+		_recent_files_submenu.add_item(path.get_file())
+		_recent_files_submenu.set_item_tooltip(_recent_files_submenu.item_count - 1, path)
+
+
+func _on_recent_file_selected(index: int) -> void:
+	if index < _recent_files_cache.size():
+		_open_file(_recent_files_cache[index])
+
+
+func _save_open_files_state() -> void:
+	if _is_config_initialized:
+		_config_manager.set_settings_value("main", "open_files", Array(_file_states.keys()))
+
+
 func _close_all() -> void:
 	if is_instance_valid(_autosave_timer):
 		_autosave_timer.stop()
 	_file_states = {}
+	_save_open_files_state()
 	_current_full_file = ""
 	_current_file = ""
 	_current_path = ""
@@ -292,6 +354,17 @@ func _on_Popup_hide() -> void:
 func _on_file_tab_changed(tab_idx: int) -> void:
 	if _switching_tabs:
 		return
+
+	if _file_tab_bar.get_tab_metadata(tab_idx) == _PLUS_TAB_META:
+		_switching_tabs = true
+		for i in range(_file_tab_bar.tab_count):
+			if _file_tab_bar.get_tab_metadata(i) == _current_full_file:
+				_file_tab_bar.current_tab = i
+				break
+		_switching_tabs = false
+		open_file_popup()
+		return
+
 	_switching_tabs = true
 
 	if not _current_full_file.is_empty() and _file_states.has(_current_full_file):
@@ -311,6 +384,9 @@ func _on_file_tab_changed(tab_idx: int) -> void:
 
 
 func _on_file_tab_close_pressed(tab_idx: int) -> void:
+	if _file_tab_bar.get_tab_metadata(tab_idx) == _PLUS_TAB_META:
+		return
+
 	var file_to_close: String = _file_tab_bar.get_tab_metadata(tab_idx)
 	var is_current: bool = file_to_close == _current_full_file
 
@@ -320,16 +396,18 @@ func _on_file_tab_close_pressed(tab_idx: int) -> void:
 	_switching_tabs = true
 	_file_tab_bar.remove_tab(tab_idx)
 
-	if _file_tab_bar.tab_count == 0:
+	if _file_tab_bar.tab_count == 1:  # only "+" tab remains
 		_switching_tabs = false
 		_close_all()
 		return
+
+	_save_open_files_state()
 
 	if is_current:
 		_current_full_file = ""
 		_autosave_timer.stop()
 		_clear_search()
-		var new_idx: int = mini(tab_idx, _file_tab_bar.tab_count - 1)
+		var new_idx: int = mini(tab_idx, _file_tab_bar.tab_count - 2)  # -2 to exclude "+"
 		_file_tab_bar.current_tab = new_idx
 		_switching_tabs = false
 		await _on_file_tab_changed(new_idx)
@@ -455,6 +533,7 @@ func _do_open_file(full_path: String, delimiter: String) -> void:
 	_update_file_tabs()
 	_set_visible_content(true)
 	_no_files_panel.add_recent_file(full_path)
+	_save_open_files_state()
 
 
 func _apply_recovery() -> void:
@@ -515,6 +594,10 @@ func _delete_temp_file(file_path: String = "") -> void:
 
 func _update_file_tabs() -> void:
 	_switching_tabs = true
+	# Remove existing "+" tab so we can re-add it at the end
+	for i in range(_file_tab_bar.tab_count - 1, -1, -1):
+		if _file_tab_bar.get_tab_metadata(i) == _PLUS_TAB_META:
+			_file_tab_bar.remove_tab(i)
 	var tabbed_paths: Array = []
 	for i in range(_file_tab_bar.tab_count):
 		tabbed_paths.append(_file_tab_bar.get_tab_metadata(i))
@@ -524,6 +607,9 @@ func _update_file_tabs() -> void:
 			var new_idx: int = _file_tab_bar.tab_count - 1
 			_file_tab_bar.set_tab_metadata(new_idx, path)
 			_file_tab_bar.set_tab_tooltip(new_idx, path)
+	_file_tab_bar.add_tab("+")
+	var plus_idx: int = _file_tab_bar.tab_count - 1
+	_file_tab_bar.set_tab_metadata(plus_idx, _PLUS_TAB_META)
 	for i in range(_file_tab_bar.tab_count):
 		if _file_tab_bar.get_tab_metadata(i) == _current_full_file:
 			_file_tab_bar.current_tab = i
@@ -664,29 +750,48 @@ func _on_language_removed(selected_lang: String) -> void:
 
 func _on_filter_changed(index: int) -> void:
 	match index:
-		0:
-			_search_filters["need_translation"] = !_search_filters["need_translation"]
 		1:
+			_search_filters["need_translation"] = !_search_filters["need_translation"]
+			_search_filter_popup.set_item_checked(1, _search_filters["need_translation"])
+		2:
 			_search_filters["need_revision"] = !_search_filters["need_revision"]
+			_search_filter_popup.set_item_checked(2, _search_filters["need_revision"])
+		4:
+			var new_val: bool = !_search_filters["search_key"]
+			if (
+				not new_val
+				and not _search_filters["search_ref_text"]
+				and not _search_filters["search_target_text"]
+			):
+				return
+			_search_filters["search_key"] = new_val
+			_search_filter_popup.set_item_checked(4, new_val)
+		5:
+			var new_val: bool = !_search_filters["search_ref_text"]
+			if (
+				not _search_filters["search_key"]
+				and not new_val
+				and not _search_filters["search_target_text"]
+			):
+				return
+			_search_filters["search_ref_text"] = new_val
+			_search_filter_popup.set_item_checked(5, new_val)
+		6:
+			var new_val: bool = !_search_filters["search_target_text"]
+			if (
+				not _search_filters["search_key"]
+				and not _search_filters["search_ref_text"]
+				and not new_val
+			):
+				return
+			_search_filters["search_target_text"] = new_val
+			_search_filter_popup.set_item_checked(6, new_val)
 		_:
 			return
 	_start_search()
 
 
 func _on_LineEditSearchBox_text_changed(_new_text: String) -> void:
-	_start_search()
-
-
-# pressed any checkbox from the search bar
-func _on_CheckBoxSearch_pressed() -> void:
-	# at least one of the checks must be pressed
-	if (
-		_cb_search_key.button_pressed == false
-		and _cb_search_ref_text.button_pressed == false
-		and _cb_search_target_text.button_pressed == false
-	):
-		_cb_search_key.button_pressed = true
-
 	_start_search()
 
 
